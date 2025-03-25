9eadc9a03bbaac9b210c56dbe316522e2c82d920
vitor gamarano <146540200+Vitor-S-G-C@users.noreply.github.com>
2025-03-25 15:48:25 -0300

    Delete orderms directory

--- orderms/src/main/java/com/example/orderms/config/RabbitMqConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.orderms.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-
-public class RabbitMqConfig {
-    public static final String ORDER_CREATED_QUEUE = "btg-pactual-order-createrd";
-}
